5352671a748dc9fc7c129bf55223e42ca692012d
Yaoming Huang <yaoming.huang@ischool.com.tw>
2022-09-13 21:07:55 +0800

    高、寬放反了。

--- designs.js
+++ designs.js
@@ -15,9 +15,9 @@ sizePicker.addEventListener('submit', function(event) {
 
 function makeGrid(height, width) {
     let table = document.createElement('table')
-        for (var w=0; w<width; w++){
+        for (var w=0; w<height; w++){
             const row = table.insertRow();
-            for (var h=0; h<height; h++){
+            for (var h=0; h<width; h++){
                 const cell = row.insertCell();
                 cell.addEventListener('click', function(event){
                     const color = pickacolor.value;
